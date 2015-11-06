76416c5d821263d02f0ca579c4dac96d3b2fb94b
marcelo.gomez <unknown@unknown.es>
2015-11-06 16:24:59 +0000

    Configuration parameters + OfferServiceFactory

---
+++ ws-app-model/src/main/java/es/udc/ws/app/model/offerservice/OfferServiceFactory.java
@@ -0,0 +1,36 @@
+package es.udc.ws.app.model.offerservice;
+
+import es.udc.ws.app.model.offerservice.OfferService;
+import es.udc.ws.app.configuration.ConfigurationParametersManager;
+
+public class OfferServiceFactory {
+
+    private final static String CLASS_NAME_PARAMETER = "OfferServiceFactory.className";
+    private static OfferService service = null;
+
+    private OfferServiceFactory() {
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static OfferService getInstance() {
+        try {
+            String serviceClassName = ConfigurationParametersManager
+                    .getParameter(CLASS_NAME_PARAMETER);
+            Class serviceClass = Class.forName(serviceClassName);
+            return (OfferService) serviceClass.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public synchronized static OfferService getService() {
+
+        if (service == null) {
+            service = getInstance();
+        }
+        return service;
+
+    }
+	
+}
