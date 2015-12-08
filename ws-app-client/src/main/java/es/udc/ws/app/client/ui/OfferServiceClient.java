4ae7730a82390dd7ee4e7b6c8c79b8d9b683c9e3
carlos.bouzon.garcia <carlos.bouzon.garcia@udc.es>
2015-12-08 16:09:43 +0000

    Added client skeleton :D

--- ws-app-client/src/main/java/es/udc/ws/app/client/ui/OfferServiceClient.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/ui/OfferServiceClient.java
@@ -1,12 +1,74 @@
 package es.udc.ws.app.client.ui;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import es.udc.ws.app.client.service.ClientOfferService;
+import es.udc.ws.app.client.service.ClientOfferServiceFactory;
+import es.udc.ws.app.client.types.*;
+
 public class OfferServiceClient {
-	
+
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			printUsageAndExit();
 		}
+
+		ClientOfferService clientOfferService = ClientOfferServiceFactory
+				.getInstance();
+
+		if ("-a".equalsIgnoreCase(args[0])) {
+			validateArgs(args, 9, new int[] {5,6,7});
+			Calendar limitReservationDate = toDate(args[2]);
+			Calendar limitApplicationDate = toDate(args[3]);
+			Offer offer = new Offer(args[0],args[1],limitReservationDate,limitApplicationDate,Float.valueOf(args[4]),Float.valueOf(args[5]),Float.valueOf(args[6]),true);
+			
+		}
 		
+		else if ("-u".equalsIgnoreCase(args[0])){
+			validateArgs(args, 10, new int[] {1,5,6,7});
+			Calendar limitReservationDate = toDate(args[3]);
+			Calendar limitApplicationDate = toDate(args[4]);
+			Offer offer = new Offer(Integer.valueOf(args[1]),args[2],args[3],limitReservationDate,limitApplicationDate,Float.valueOf(args[5]),Float.valueOf(args[6]),Float.valueOf(args[7]),true);
+		}
+		
+		else if ("-r".equalsIgnoreCase(args[0])){
+			validateArgs(args,2,new int[] {1});
+		}
+		
+		else if ("-g".equalsIgnoreCase(args[0])){
+			validateArgs(args,2,new int[] {1});
+		}
+		
+		else if ("-f".equalsIgnoreCase(args[0])){
+			validateArgs(args,2,new int[] {});
+		}
+		
+		else if ("-reserve".equalsIgnoreCase(args[0])){
+			validateArgs(args,4,new int[] {1});
+		}
+		
+		else if ("-claim".equalsIgnoreCase(args[0])){
+			validateArgs(args,3,new int[] {1});
+		}
+		
+		else if ("-getReservation".equalsIgnoreCase(args[0])){
+			validateArgs(args,2,new int[] {});
+		}
+		
+		else if ("-findUserReservation".equalsIgnoreCase(args[0])){
+			validateArgs(args,3,new int[] {});
+		}
+		
+		else if ("-getUserOffers".equalsIgnoreCase(args[0])){
+			validateArgs(args,2,new int[] {});
+		}
+		
+		else {
+			printUsageAndExit();
+		}
 		
 	}
 
@@ -16,7 +78,7 @@ public class OfferServiceClient {
 		if (expectedArgs != args.length) {
 			printUsageAndExit();
 		}
-		
+
 		for (int i = 0; i < numericArguments.length; i++) {
 			int position = numericArguments[i];
 			try {
@@ -35,6 +97,23 @@ public class OfferServiceClient {
 	private static void printUsage() {
 		System.out.println("Procedure is being constructed");
 	}
-	 
-	 
+	
+	private static Calendar toDate(String dateString){
+		Calendar cal = Calendar.getInstance();
+		DateFormat format= new SimpleDateFormat("dd-MM-yyyy'T'HH:mm");
+		try {
+			cal.setTime(format.parse(dateString));
+		} catch (ParseException e) {
+			printDateFormatAndExit();
+		}
+		return cal;
+	}
+	
+	private static void printDateFormatAndExit() {
+		printDateFormat();
+	}
+
+	private static void printDateFormat() {
+		System.exit(-1);
+	}
 }
