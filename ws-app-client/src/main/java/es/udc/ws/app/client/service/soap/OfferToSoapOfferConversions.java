fc487c8ce31ebbd3b85add6daa702323afcaf0e5
carlos.bouzon.garcia <carlos.bouzon.garcia@udc.es>
2015-12-08 17:32:57 +0000

    Added all offers soap conversions.

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/soap/OfferToSoapOfferConversions.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/soap/OfferToSoapOfferConversions.java
@@ -47,4 +47,43 @@ public class OfferToSoapOfferConversions {
 		soapOffer.setValid(offer.isValid());
 		return soapOffer;
 	}
+
+	public es.udc.ws.app.client.types.OfferDto toClientOfferDto(es.udc.ws.app.client.service.soap.wsdl.OfferDto offer){
+		Calendar limitReservationDate = Calendar.getInstance();
+		limitReservationDate.setTime(offer.getLimitReservationDate().toGregorianCalendar().getTime());
+		Calendar limitApplicationDate = Calendar.getInstance();
+		limitApplicationDate.setTime(offer.getLimitReservationDate().toGregorianCalendar().getTime());
+		return new es.udc.ws.app.client.types.OfferDto (offer.getOfferId(),offer.getName(),offer.getDescription(),limitReservationDate,limitApplicationDate,offer.getRealPrice(),offer.getDiscountedPrice(),offer.isValid());
+	}
+	
+	public es.udc.ws.app.client.service.soap.wsdl.OfferDto toSoapOfferDto(es.udc.ws.app.client.types.OfferDto offer) {
+		XMLGregorianCalendar limitReservationDate = null;
+		XMLGregorianCalendar limitApplicationDate = null;
+		GregorianCalendar aux = null;
+		
+		try {
+			aux = new GregorianCalendar();
+			aux.setTime(offer.getLimitReservationDate().getTime());
+			limitReservationDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(aux);
+			aux = new GregorianCalendar();
+			aux.setTime(offer.getLimitReservationDate().getTime());
+			limitApplicationDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(aux);
+		} catch (DatatypeConfigurationException e) {
+			System.err.println("Error parsing date to xml date.");
+			e.printStackTrace();
+		}
+		es.udc.ws.app.client.service.soap.wsdl.OfferDto soapOffer =  new es.udc.ws.app.client.service.soap.wsdl.OfferDto();
+		soapOffer.setOfferId(offer.getOfferId());
+		soapOffer.setName(offer.getName());
+		soapOffer.setDescription(offer.getDescription());
+		soapOffer.setLimitReservationDate(limitReservationDate);
+		soapOffer.setLimitApplicationDate(limitApplicationDate);
+		soapOffer.setRealPrice(offer.getRealPrice());
+		soapOffer.setDiscountedPrice(offer.getDiscountedPrice());
+		soapOffer.setValid(offer.isValid());
+		return soapOffer;
+	}
+	
+	
+	
 }
